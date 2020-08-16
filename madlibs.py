378a381ecdbe62c631b8c0a96914e22aa0840c74
ShahdAllemie0 <69586588+ShahdAllemie0@users.noreply.github.com>
2020-08-16 14:55:12 +0300

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,13 +1,13 @@
 def main():
 	# write your code here
-	time= input("Enter a number from 1 to 12: ")
+	time= input("Enter a number from 1 to 12:")
 	time2=int(time)
-	items= input("Enter a noun (plural): ")
+	items= input("Enter a noun (plural):")
 	name=input("Enter a name: ")
 	name2=name.capitalize()
-	scream=input("Enter any sentence: ")
+	scream=input("Enter any sentence:")
 	scream2=scream.upper()
-	v=input("Enter a verb: ")
+	v=input("Enter a verb:")
 	print()
 	print("The story goes...")
 	print()
@@ -17,9 +17,10 @@ def main():
 
 
 
-	print("It was %d o'clock when I heard a knock at the door. I opened the door and there was a box full of %s with a note saying From Mr. %s. Just as I closed the door I heard a scream %s I froze in place and all I could do was %s."%(time2,items,name2,scream2,v))
-
-
+	print("It was %d o'clock when I heard a knock at the door."%(time2))
+	print( "I opened the door and there was a box full of %s with a note saying \"From Mr. %s.\""%(items,name2))
+	print("Just as I closed the door I heard a scream \"%s.\" "%(scream2))
+	print("I froze in place and all I could do was %s."%(v))
 
 
 
